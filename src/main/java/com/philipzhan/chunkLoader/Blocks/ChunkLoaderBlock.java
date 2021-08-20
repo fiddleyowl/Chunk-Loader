cc917815d5509f859214f2831d5a4710c1b888ed
Philip <hhhphilip@icloud.com>
2021-08-20 21:46:34 +0800

    Add tick function

--- src/main/java/com/philipzhan/chunkLoader/Blocks/ChunkLoaderBlock.java
+++ src/main/java/com/philipzhan/chunkLoader/Blocks/ChunkLoaderBlock.java
@@ -1,25 +1,21 @@
 package com.philipzhan.chunkLoader.Blocks;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.chunk.WorldChunk;
 import org.jetbrains.annotations.Nullable;
 
 public class ChunkLoaderBlock extends Block {
+
     public ChunkLoaderBlock(Settings settings) {
         super(settings);
     }
@@ -27,7 +23,6 @@ public class ChunkLoaderBlock extends Block {
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
         super.onPlaced(world, pos, state, placer, itemStack);
-        System.out.println("Placed one chunk loader block.");
 
         int chunkX = world.getChunk(pos).getPos().x;
         int chunkZ = world.getChunk(pos).getPos().z;
@@ -37,7 +32,30 @@ public class ChunkLoaderBlock extends Block {
             System.out.println("Set server chunk forced at x " + chunkX + ", z " + chunkZ);
             MinecraftServer server = world.getServer();
             ServerWorld serverWorld = server.getOverworld();
+            WorldChunk worldChunk = serverWorld.getChunk(chunkX,chunkZ);
             serverWorld.setChunkForced(chunkX, chunkZ,true);
+            ServerTickEvents.START_SERVER_TICK.register((listener) -> {
+                int tickSpeed = serverWorld.getGameRules().getInt(GameRules.RANDOM_TICK_SPEED);
+                serverWorld.tickChunk(worldChunk, tickSpeed);
+                System.out.println("Ticked");
+            });
         }
     }
+
+    @Override
+    public void onBroken(WorldAccess world, BlockPos pos, BlockState state) {
+        super.onBroken(world, pos, state);
+
+        int chunkX = world.getChunk(pos).getPos().x;
+        int chunkZ = world.getChunk(pos).getPos().z;
+
+        if (!world.isClient()) {
+            // is server
+            System.out.println("Removed server forced chunk at x " + chunkX + ", z " + chunkZ);
+            MinecraftServer server = world.getServer();
+            ServerWorld serverWorld = server.getOverworld();
+            serverWorld.setChunkForced(chunkX, chunkZ,false);
+        }
+    }
+
 }
